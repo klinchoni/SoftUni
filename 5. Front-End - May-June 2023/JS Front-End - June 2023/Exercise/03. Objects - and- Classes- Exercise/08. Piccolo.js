f5128a009070db6adb589e561bc3758a0561d63f
klinchoni <klinchovay@gmail.com>
2023-07-12 14:06:17 +0300

    update 08.Piccolo.js

--- 5. Front-End - May-June 2023/JS Front-End - June 2023/Exercise/03. Objects - and- Classes- Exercise/08. Piccolo.js
+++ 5. Front-End - May-June 2023/JS Front-End - June 2023/Exercise/03. Objects - and- Classes- Exercise/08. Piccolo.js
@@ -37,6 +37,50 @@ function solve(input) {
   }
 
   // JUDGE 100/100
+  function piccolo(parkingLot) {
+    class Parking {
+      constructor() {
+        this.cars = new Set();
+      }
+      enter(carNumber) {
+        this.cars.add(carNumber);
+      }
+      exit(carNumber) {
+        this.cars.delete(carNumber);
+      }
+      printCars() {
+        this.cars.size === 0
+          ? console.log("Parking Lot is Empty")
+          : Array.from(this.cars)
+              .sort()
+              .forEach((carNumber) => {
+                console.log(carNumber);
+              });
+      }
+    }
+    const parking = new Parking();
+  
+    parkingLot.forEach((entry) => {
+      const [direction, carNumber] = entry.split(", ");
+      direction === "IN" ? parking.enter(carNumber) : parking.exit(carNumber);
+    });
+  
+    parking.printCars();
+  }
+
+ //piccolo([
+ //  "IN, CA2844AA",
+ //  "IN, CA1234TA",
+ //  "OUT, CA2844AA",
+ //  "IN, CA9999TT",
+ //  "IN, CA2866HI",
+ //  "OUT, CA1234TA",
+ //  "IN, CA2844AA",
+ //  "OUT, CA2866HI",
+ //  "IN, CA9876HH",
+ //  "IN, CA2822UU",
+ //]);
+  // JUDGE 100/100
   function solve(input) {
     let parking = [];
     input.forEach((element) => {
@@ -59,7 +103,7 @@ function solve(input) {
       console.log(parking.sort().join("\n"));
     }
   }
-  
+
   solve([
     "IN, CA2844AA",
     "IN, CA1234TA",
