394774e1803af939fae3653985e2cdb36c84ff62
klinchoni <klinchovay@gmail.com>
2023-07-22 11:11:43 +0300

    update 01.Edit Element

--- 5. Front-End - May-June 2023/JS Front-End - June 2023/More Exercise/04. DOM - and - Events - More Exercises/01. Edit-Element/EditElement.js
+++ 5. Front-End - May-June 2023/JS Front-End - June 2023/More Exercise/04. DOM - and - Events - More Exercises/01. Edit-Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(reference, match, replacer) {
+    const inputRef = document.querySelector("#e1");
+    const input= reference.textContent;
+    reference.textContent = input.replace(new RegExp(match, 'g'), replacer);
 }
